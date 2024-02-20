98a48d2797dedbb86a03d3760e6937f34f4153b3
Wang <wang.rui@siemens.com>
2024-02-20 15:27:13 +0800

    base

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Ultra-Gobi-Time-Reduction-Calculator/' // 为/仓库名/
 })
